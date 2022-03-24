9064d691528038f6c65be9f61e2e1f71071f87ab
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-24 16:12:37 -0300

    create home page layout

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,31 @@ import styles from './Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={ `container ${styles.container}` }>
-      mozelli blog
+    <div className="container">
+      <div className={styles.homeLayout}>
+        <div className={ styles.header }>
+
+        </div>
+        <div className={ styles.feed }>
+          <div className={ styles.post }>
+
+          </div>
+        </div>
+        <div className={ styles.footer }>
+          <div className={ styles.footerColum }>
+
+          </div>
+          <div className={ styles.footerColum }>
+            
+          </div>
+          <div className={ styles.footerColum }>
+            
+          </div>
+          <div className={ styles.footerCredits }>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
