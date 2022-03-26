58bae2abd2cf45b2449a33f8d579289198f0ea35
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-26 16:54:11 -0300

    fixing wrong path to favicons in html > head

--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,10 @@ class MyDocument extends Document {
     return (
         <Html>
             <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
             </Head>
             <body>
