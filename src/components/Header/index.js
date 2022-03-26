4809d6db9f405a11de3cae769c2e8405f63c00e7
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-26 16:06:29 -0300

    create Header component styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,11 @@ import styles from './header.module.scss';
 
 export default function Header() {
     return(
-        <header>
+        <header className={ styles.headerComponent }>
             <h1>mozelli blog</h1>
+            <button className="buttonMenu" type='button'>
+                <i className="ri-menu-fill ri-xl"></i>
+            </button>
         </header>
     );
 }
