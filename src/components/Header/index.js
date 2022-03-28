902de64ccaf7e050a0ec6874f60b502b8080eeef
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-28 11:34:46 -0300

    creating responsive navBar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,23 @@
 import styles from './header.module.scss';
 
 export default function Header() {
+    
     return(
         <header className={ styles.headerComponent }>
-            <h1>mozelli blog</h1>
-            <button className="buttonMenu" type='button'>
-                <i className="ri-menu-fill ri-xl"></i>
-            </button>
+            <div className={ styles.content }>
+                <h1>mozelli blog</h1>
+                <button className={ styles.menuButton }>
+                    <i className="ri-menu-fill ri-xl"></i>
+                </button>
+                <nav className={styles.navBar} id={'open'}>
+                    <ul>
+                        <li>Home</li>
+                        <li>Sobre</li>
+                        <li>Inscreva-se!</li>
+                    </ul>
+                </nav>
+            </div>
+            
         </header>
     );
 }
