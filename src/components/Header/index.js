09d0d8214f39cad5fd5cadf6db21f2bebf7f0b47
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-28 14:23:36 -0300

    create collapse menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,48 @@
+import { useState } from 'react';
 import styles from './header.module.scss';
 
 export default function Header() {
+
+    const [navBarCollapseShow, setNavBarCollapseShow] = useState(true)
+    const [navBarCollapseStyle, setNavBarCollapseStyle] = useState(styles.navBarCollapse);
+
+    function changeNavBarCollapseShowState() {
+        setNavBarCollapseShow(!navBarCollapseShow);
+
+        if(navBarCollapseShow) {
+            setNavBarCollapseStyle(`${styles.navBarCollapse} ${styles.show}`);
+        } else {
+            setNavBarCollapseStyle(styles.navBarCollapse);
+        }
+
+    }
     
     return(
-        <header className={ styles.headerComponent }>
-            <div className={ styles.content }>
-                <h1>mozelli blog</h1>
-                <button className={ styles.menuButton }>
-                    <i className="ri-menu-fill ri-xl"></i>
-                </button>
-                <nav className={styles.navBar} id={'open'}>
+        <nav className={ styles.headerComponent }>
+            <div className={ styles.containerFluid }>
+                
+                <div className={ styles.navBar }>
+                    <h1>mozelli blog</h1>
+                    <input 
+                        type="checkbox" 
+                        className={ styles.mobileButtonMenu } 
+                        id="mobileButtonMenu"
+                        onChange={ changeNavBarCollapseShowState } 
+                    />
+                    <label htmlFor="mobileButtonMenu" className={ styles.mobileButtonMenuIcon }>
+                        <i className="ri-menu-line ri-xl"></i>
+                    </label>
+                </div>
+                
+                <div className={ navBarCollapseStyle }>
                     <ul>
                         <li>Home</li>
                         <li>Sobre</li>
-                        <li>Inscreva-se!</li>
+                        <li>Inscreva-se</li>
                     </ul>
-                </nav>
+                </div>
             </div>
             
-        </header>
+        </nav>
     );
 }
