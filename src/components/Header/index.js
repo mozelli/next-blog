2f47ea93597b808bb6a5bf6b43d3f5543e5022c9
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-28 15:13:08 -0300

    fixing styles alignment

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header() {
@@ -36,9 +37,30 @@ export default function Header() {
                 
                 <div className={ navBarCollapseStyle }>
                     <ul>
-                        <li>Home</li>
-                        <li>Sobre</li>
-                        <li>Inscreva-se</li>
+                        <Link href={'/'} alt="Home">
+                            <a>
+                                <li>
+                                <i className="ri-home-2-fill ri-xl"></i> Home
+                                </li>
+                            </a>
+                        </Link>
+                        <Link href={'/'} alt="Sobre">
+                            <a>
+                                <li>
+                                    <i className="ri-file-info-fill ri-xl"></i> Sobre
+                                </li>
+                            </a>
+                        </Link>
+                        <Link href={'/'} alt="Inscreva-se">
+                            <a>
+                                <li>
+                                <i className="ri-mail-star-fill ri-xl"></i> Inscreva-se
+                                </li>
+                            </a>
+                        </Link>
+
+                        
+                        
                     </ul>
                 </div>
             </div>
